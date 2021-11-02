6faf602b64fb115412365738efe30dc1f758afda
Rajat Mhetre <mhetrerajat@gmail.com>
2021-11-03 00:30:12 +0530

    feat(io): add function to load stored data

--- core/io.py
+++ core/io.py
@@ -2,15 +2,17 @@ import json
 import os
 import tempfile
 from enum import Enum
-from typing import Dict
+from pathlib import Path
+from typing import Dict, List, Union
 from uuid import uuid4
 
 
 class DataType(Enum):
     YOUTUBE_SEARCH = "ytsearch"
+    YOUTUBE_VIDEO = "ytvideo"
 
 
-def dump(data: Dict, data_type: DataType) -> str:
+def dump(data: Union[Dict, List], data_type: DataType) -> str:
     """Write data in specified directory in /tmp/"""
 
     DATA_DIR = tempfile.mkdtemp(dir="/tmp/", prefix=f"aviyel__{data_type.value}__")
@@ -21,3 +23,12 @@ def dump(data: Dict, data_type: DataType) -> str:
         f.write(json.dumps(data))
 
     return path
+
+
+def loads(data_type: DataType) -> str:
+    """Loads all files one by one for specified data type"""
+    dirs = Path("/tmp/").glob(f"aviyel__{data_type.value}*")
+    for dir in dirs:
+        for path in dir.glob("**/*"):
+            with open(path, "r") as f:
+                yield json.loads(f.read())
